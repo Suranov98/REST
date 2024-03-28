18926f310ffd512af010c94bbfffb798b2626b1d
ArtSur <104309491+Suranov98@users.noreply.github.com>
2024-03-28 22:57:08 +0500

    Add files via upload

---
+++ RESTAPI_test-DoneSixthLab/src/main/java/com/example/testthymeleafwebapp/entity/Student.java
@@ -0,0 +1,39 @@
+package com.example.testthymeleafwebapp.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Entity
+@ToString
+@AllArgsConstructor
+@Table(name="STUDENTS")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="surname")
+    private String surname;
+
+    @Column(name="faculty")
+    private String faculty;
+
+    @Column(name="age")
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(String name, String surname, String faculty, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.faculty = faculty;
+        this.age = age;
+    }
+}
